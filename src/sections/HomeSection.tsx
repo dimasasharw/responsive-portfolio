1adf27341774e7958368517a5f0f2c56a16272ba
Dimas Ashar <dimas.ashar1997@gmail.com>
2025-09-08 01:33:09 +0700

    Fix ESLint error for deployment

--- src/sections/HomeSection.tsx
+++ src/sections/HomeSection.tsx
@@ -2,15 +2,13 @@ import { Text } from "@/components/core-ui/Text";
 import React from "react";
 
 export default function HomeSection() {
-  return (
+ return (
     <div>
       <h1
         className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
-        // initial={{ opacity: 0, y: 100 }}
-        // animate={{ opacity: 1, y: 0 }}
       >
         <Text color="white">
-          <span className="font-bold">Hello, I'm Dimas.</span> I'm a{" "}
+          <span className="font-bold">Hello, I&apos;m Dimas.</span> I&apos;m a{" "}
           <span className="font-bold">full-stack developer</span> with{" "}
           <span className="font-bold">almost 2 years</span> of experience in
           Frontend. I enjoy building{" "}
