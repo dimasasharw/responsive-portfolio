b486fb2b1a9f8185aed320048d545beb89a0d057
Dimas Ashar <dimas.ashar1997@gmail.com>
2025-11-26 15:52:18 +0700

    nav and section

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,8 +7,10 @@ export default function Navbar() {
   return (
     <div className="flex w-full h-[4rem] justify-center fixed top-0 border-b border-b-[#444444] bg-black/90">
       <div className="flex flex-row items-center gap-5">
-        {MENUS.map((item, index) => (
-          <Text key={index}>{item?.label}</Text>
+        {MENUS.map((item) => (
+          <a href={item.path} key={item.key}>
+            <Text>{item?.label}</Text>
+          </a>
         ))}
       </div>
     </div>
--- src/sections/AboutSection.tsx
+++ src/sections/AboutSection.tsx
@@ -5,7 +5,7 @@ import { Text } from "@/components/core-ui/Text";
 
 export function AboutSection() {
   return (
-    <div className="flex flex-col items-center w-full">
+    <div id="about" className="flex flex-col items-center w-full">
       <Image
         src="assets/GIF/underConstruction.gif"
         alt="Coding animation"
@@ -14,7 +14,18 @@ export function AboutSection() {
         unoptimized
       />
       <h1 className="text-white text-2xl font-bold mt-4">
-        <Text>This Section is Under Construction</Text>
+        <Text>
+          After shifting my focus from a non-tech background, I discovered my
+          passion for web development. I enjoy the balance between creativity
+          and logic that comes with building applications. My main stack is
+          React, TypeScript, and Tailwind CSS, and I often use Redux Toolkit for
+          state management. I also love working with data visualization tools
+          like ApexCharts. I’m always eager to learn new technologies and
+          improve the way I build things. Outside of coding, I continuously seek
+          to refine my workflow and stay updated with modern development
+          practices. I value clean, maintainable code and enjoy collaborating on
+          projects that emphasize both technical quality and user experience.
+        </Text>
       </h1>
     </div>
   );
